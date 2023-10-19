3bf06b8d82f02c628a683fd6000f8406d675cefc
TKCoder13 <tkcoding13@gmail.com>
2023-10-19 15:39:17 -0700

    Finish Init of CN.cpp w/o toString()

--- C++/ComplexNumber.cpp
+++ C++/ComplexNumber.cpp
@@ -80,17 +80,44 @@ ComplexNumber ComplexNumber::div(ComplexNumber rhs) {
 } 
 
 double ComplexNumber::mag() {
-
+    double output = std::sqrt((this->real * this->real) + (this->imag * this->imag));
+    return output;
 }
 
 ComplexNumber ComplexNumber::conj() {
-
+    double imagOutput = 0 - this->imag;
+    ComplexNumber output(this->real, imagOutput);
 }
 
 ComplexNumber ComplexNumber::sqrt() {
-
+    // -- sqrt(-1) = i
+    // -- sqrt(i) = -1
+    if (this->real < 0 && this->imag < 0) {
+        double realOutput = std::sqrt(std::abs(this->imag));
+        double imagOutput = std::sqrt(std::abs(this->real));
+        ComplexNumber output(realOutput, imagOutput);
+        return output;
+    } else if (this->real < 0) {
+        double realOutput = 0;
+        double imagOutput = std::sqrt(std::abs(this->real)) + std::sqrt(this->imag);
+        ComplexNumber output(realOutput, imagOutput);
+        return output;
+    } else if (this->imag < 0) {
+        double realOutput = std::sqrt(this->real) + std::sqrt(std::abs(this->imag));
+        double imagOutput = 0;
+        ComplexNumber output(realOutput, imagOutput);
+        return output;
+    }
+    double realOutput = std::sqrt(this->real);
+    double imagOutput = std::sqrt(this->imag);
+    ComplexNumber output(realOutput, imagOutput);
+    return output;
 }
 
 bool ComplexNumber::equals(ComplexNumber& rhs) {
-
+    if (typeid(*this) != typeid(rhs))
+        return false;
+    if (this->real == rhs.real && this->imag == rhs.imag)
+        return true;
+    return false;
 }
